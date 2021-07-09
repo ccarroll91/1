1ff291f571d11863ca9fd2c9f5e54ee999dedf1a
ccarroll91 <conor-carroll@hotmail.com>
2021-07-09 14:31:08 +0000

    Full audio test

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -32,145 +32,162 @@ const sounds = [
 
 document.getElementById('play1').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio2').pause();
     document.getElementById('audio1').play();
   });
   
   document.getElementById('play2').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio1').pause();
+    document.getElementById('audio1', 'audio3').pause();
     document.getElementById('audio2').play();
   });
   
   document.getElementById('play3').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio2').pause();
+    document.getElementById('audio2', 'audio4').pause();
     document.getElementById('audio3').play();
   });
   
   document.getElementById('play4').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio3').pause();
+    document.getElementById('audio3', 'audio5').pause();
     document.getElementById('audio4').play();
   });
   
   document.getElementById('play5').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio4').pause();
+    document.getElementById('audio4', 'audio6').pause();
     document.getElementById('audio5').play();
   });
   
   document.getElementById('play6').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio5').pause();
+    document.getElementById('audio5', 'audio7').pause();
     document.getElementById('audio6').play();
   });
   
   document.getElementById('play7').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio6').pause();
+    document.getElementById('audio6', 'audio8').pause();
     document.getElementById('audio7').play();
   });
   
   document.getElementById('play8').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio7').pause();
+    document.getElementById('audio7', 'audio9').pause();
     document.getElementById('audio8').play();
   });
   
   document.getElementById('play9').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio8').pause();
+    document.getElementById('audio8', 'audio10').pause();
     document.getElementById('audio9').play();
   });
   
   document.getElementById('play10').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio9').pause();
+    document.getElementById('audio9', 'audio11').pause();
     document.getElementById('audio10').play();
   });
   
   document.getElementById('play11').addEventListener('click', function (e) {
     e.preventDefault();
-    document.getElementById('audio10').pause();
+    document.getElementById('audio10', 'audio12').pause();
     document.getElementById('audio11').play();
   });
   
   document.getElementById('play12').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio11', 'audio13').pause();
     document.getElementById('audio12').play();
   });
   
   document.getElementById('play13').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio12', 'audio14').pause();
     document.getElementById('audio13').play();
   });
   
   document.getElementById('play14').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio13', 'audio15').pause();
     document.getElementById('audio14').play();
   });
   
   document.getElementById('play15').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio14', 'audio16').pause();
     document.getElementById('audio15').play();
   });
   
   document.getElementById('play16').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio15', 'audio17').pause();
     document.getElementById('audio16').play();
   });
   
   document.getElementById('play17').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio16', 'audio18').pause();
     document.getElementById('audio17').play();
   });
   
   document.getElementById('play18').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio17', 'audio19').pause();
     document.getElementById('audio18').play();
   });
   
   document.getElementById('play19').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio18', 'audio20').pause();
     document.getElementById('audio19').play();
   });
   
   document.getElementById('play20').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio19', 'audio21').pause();
     document.getElementById('audio20').play();
   });
   
   document.getElementById('play21').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio20', 'audio22').pause();
     document.getElementById('audio21').play();
   });
   
   document.getElementById('play22').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio21', 'audio23').pause();
     document.getElementById('audio22').play();
   });
   
   document.getElementById('play23').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio22', 'audio24').pause();
     document.getElementById('audio23').play();
   });
   
   document.getElementById('play24').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio23', 'audio25').pause();
     document.getElementById('audio24').play();
   });
   
   document.getElementById('play25').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio24', 'audio26').pause();
     document.getElementById('audio25').play();
   });
   
   document.getElementById('play26').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio25', 'audio27').pause();
     document.getElementById('audio26').play();
   });
   
   document.getElementById('play27').addEventListener('click', function (e) {
     e.preventDefault();
+    document.getElementById('audio26').pause();
     document.getElementById('audio27').play();
   });
